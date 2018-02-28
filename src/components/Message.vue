350f58740356b090221d21b3908510389fcd37de
WEI WANG <ww1306@nyu.edu>
2018-02-28 14:02:27 -0500

    add message

---
+++ src/components/Message.vue
@@ -0,0 +1,24 @@
+<template>
+   <div class="message">
+    <p>{{message}}</p>
+    <input v-model="message"/>
+  </div> 
+</template>
+
+<script>
+export default {
+    name: "Message",
+    data(){
+        return{
+            message:"My First Component"
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .message {
+        color: blueviolet
+    }
+</style>
+
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
-
+import Message from '@/components/Message'
 Vue.use(Router)
 
 export default new Router({
@@ -10,6 +10,11 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/Message',
+      name :'Message',
+      component: Message
     }
   ]
 })
